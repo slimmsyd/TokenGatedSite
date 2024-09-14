b6eadfd4b8f6a74c24e5ad7b28d5e8b051409947
Sydney Sanders <ssanderss444@gmail.com>
2024-09-14 18:21:04 -0400

    lowerd it to 90%

--- my-app/src/app/components/popup.tsx
+++ my-app/src/app/components/popup.tsx
@@ -32,7 +32,7 @@ export default function Popup({
           {"EXIT()"}
         </button>
 
-        <div className="flex flex-col gap-[25px] w-[90%] md:w-[100%] md:flex-row items-center  container-border">
+        <div className="flex flex-col gap-[25px] w-[70%] md:w-[100%] md:flex-row items-center  container-border">
           <div className="flex flex-col w-[100%]">
             <div className="p-3 border-bottom-dashed text-[20px]">
               <span>{value.title} </span>
